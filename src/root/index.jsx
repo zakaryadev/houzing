ebfcc91c754d6345f2a04e74eac2fad2c45b5554
Zakaryadev <zakaryamuratbev13@gmail.com>
2022-11-13 23:07:09 +0500

    13112022

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 const Index = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<h1>home</h1>} />
+        <Route path='/properties' element={<h1>properties</h1>} />
+        <Route path='/*' element={<h1>404 NOT FOUND</h1>} />
+        <Route path='/' element={<Navigate to={'/Home'} />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
